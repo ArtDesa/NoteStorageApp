67aba3b015eab3614c349db0d1a1bb8f78b29c96
Arthur Desa <artrdesa@outlook.com>
2025-03-29 21:55:29 -0400

    Added comments to app.module.ts with instructions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -56,3 +56,29 @@ const routes: Routes = [
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+/*
+NOTE: DO NOT FIX VULNERABILITIES WITH THE npm audit commmand, THIS WILL BREAK THE PROJECT
+YOU WILL NEED TO DELETE THE FOLDER AND GIT CLONE IT AGAIN FROM GITHUB
+
+Before running the application, you need to start the JSON server (using db.json) with: json-server --watch db.json
+This will launch the backend at a default URL, typically http://localhost:3000.
+*Note: This will require the terminal used to run the command (the terminal will be busy running the server to take any other commands)
+You must open another terminal to run the ng serve to start the Application.
+
+ng command - Use to run application from the VS Code terminal. 
+Run project from terminal with: ng serve
+Stop the app when it is running with (in the terminal): Ctrl+C
+
+This starts the Angular development server and compiles your app. By default, it runs on http://localhost:4200.
+Open your browser and navigate to http://localhost:4200 to view your Angular app.
+
+ng command is part of the Angular CLI (Command Line Interface).
+
+Make sure the Angular CLI (Command Line Interface) is installed with: ng -version (if it is present)
+If not installed, install it with: npm install -g @angular/cli
+
+(npm commands are part of Node.js and stand for Node Package Manager. 
+npm is used to install, manage, and update the dependencies (libraries, tools, etc.) required by your Angular project.))
+
+To Create an Angular project use: ng new <my-angular-app>
+*/
